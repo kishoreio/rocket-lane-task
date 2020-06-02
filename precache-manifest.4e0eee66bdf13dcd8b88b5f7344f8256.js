c548e8b59d096a062027c68b3ef8e5af21fd3f3b
Kishore S <30261988+kishoreio@users.noreply.github.com>
2020-06-02 09:14:38 +0530

    Updates

---
+++ precache-manifest.4e0eee66bdf13dcd8b88b5f7344f8256.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8203b829d1166fec813988cbaf77c4a0",
+    "url": "/rocket-lane-task/index.html"
+  },
+  {
+    "revision": "fb64bfcfce71e93f25f5",
+    "url": "/rocket-lane-task/static/css/main.0c6213dc.chunk.css"
+  },
+  {
+    "revision": "7b759b19d7bf290b57fc",
+    "url": "/rocket-lane-task/static/js/2.297341f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rocket-lane-task/static/js/2.297341f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb64bfcfce71e93f25f5",
+    "url": "/rocket-lane-task/static/js/main.51fd8f64.chunk.js"
+  },
+  {
+    "revision": "91767686b509d2f3177f",
+    "url": "/rocket-lane-task/static/js/runtime-main.6e4422c0.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/rocket-lane-task/precache-manifest.4e0eee66bdf13dcd8b88b5f7344f8256.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rocket-lane-task/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
